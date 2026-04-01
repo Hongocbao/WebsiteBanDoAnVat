486521ef3130fd538420b6dcfa039d209229bd03
HoNgocBao <Ngocbao11a7locan2021@gmail.com>
2026-04-01 21:45:34 +0700

    chia cac phien

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebsiteBanDoAnVat.Models;
 
@@ -13,7 +13,32 @@ namespace WebsiteBanDoAnVat.Controllers
             _logger = logger;
         }
 
+        // 1. Trang Chủ
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 2. Trang Giới Thiệu
+        public IActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        // 3. Trang Sản Phẩm
+        public IActionResult SanPham()
+        {
+            return View();
+        }
+
+        // 4. Trang Tin Tức
+        public IActionResult TinTuc()
+        {
+            return View();
+        }
+
+        // 5. Trang Liên Hệ
+        public IActionResult LienHe()
         {
             return View();
         }
